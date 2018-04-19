24c13813c8b3b0f41aff0035203a6b77e6fd21f3
tbcrozier <20345197+tbcrozier@users.noreply.github.com>
2018-04-18 21:53:01 -0500

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,29 +1,45 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template,  request, session
 import sqlite3 as sql
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('home.html')
+   return render_template('login.html')
 
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True     
+        return render_template('home.html')
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
 
 @app.route('/addrec',methods = ['POST', 'GET'])
 def addrec():
    if request.method == 'POST':
       try:
-         id1 = request.form['id']
-         email1 = request.form['email']
-         first_name1 = request.form['first_name']
-         middle_name1 = request.form['middle_name']
-         last_name1 = request.form['last_name']
-         student_id1 = request.form['student_id']
-         user_login_id1 = request.form['user_login_id']
-
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
          
          with sql.connect("database.db") as con:
             cur = con.cursor()
             
-            cur.execute("INSERT INTO Person (id,email,first_name,middle_name,last_name,student_id,user_login_id) VALUES (?,?,?,?,?,?,?)",(id1,email1,first_name1,middle_name1,last_name1,student_id1,user_login_id1) )
+            #cur.execute("""CREATE TABLE students(name, text, addr, text, city text, pin integer)""")
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
             
             con.commit()
             msg = "Record successfully added"
@@ -41,10 +57,12 @@ def list():
    con.row_factory = sql.Row
    
    cur = con.cursor()
-   cur.execute("select * from Person")
+   cur.execute("select * from students")
    
    rows = cur.fetchall();
    return render_template("list.html",rows = rows)
 
 if __name__ == '__main__':
+   app.secret_key = os.urandom(12)
    app.run(debug = True)
+
